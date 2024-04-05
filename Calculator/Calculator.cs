9e5b0c2b6a3cca5788191b8462d2c5175d3539b5
matthewsblakea <matthews.blake.a@gmail.com>
2024-04-05 14:06:23 -0400

    adding existing local repo to remote

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+
+        public static int[] GetNumbers()
+        {
+            while (true)
+            {
+                var number1 = Console.ReadLine();
+                var number2 = Console.ReadLine();
+                if (int.TryParse(number1, out int numOne) && int.TryParse(number2, out int numTwo))
+                {
+                    int[] operationNumbers = [numOne, numTwo];
+                    return operationNumbers;
+
+                }
+                else
+                {
+                    Console.WriteLine("One or more of the numbers is not an int. Please input 2 numbers");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division. 5 to quit program");
+
+                var input = Console.ReadLine();
+                var calculator = new Calculator();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("Enter 2 integers to add");
+                        int[] additionNumbers = Calculator.GetNumbers();
+                        Console.Write($"{additionNumbers[0]} + {additionNumbers[1]} = ");
+                        Console.Write(calculator.Add(additionNumbers[0], additionNumbers[1]));
+                        Console.WriteLine();
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Enter 2 integers to subtract");
+                        int[] subtractionNumbers = Calculator.GetNumbers();
+                        Console.Write($"{subtractionNumbers[0]} - {subtractionNumbers[1]} = ");
+                        Console.Write(calculator.Subtract(subtractionNumbers[0], subtractionNumbers[1]));
+                        Console.WriteLine();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Enter 2 numbers to multiply");
+                        int[] multiplicationNumbers = Calculator.GetNumbers();
+                        Console.Write($"{multiplicationNumbers[0]} * {multiplicationNumbers[1]} = ");
+                        Console.Write(calculator.Multiply(multiplicationNumbers[0], multiplicationNumbers[1]));
+                        Console.WriteLine();
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Enter 2 integers to divide");
+                        int[] divisionNumbers = Calculator.GetNumbers();
+                        Console.Write($"{divisionNumbers[0]} / {divisionNumbers[1]} = ");
+                        Console.Write(calculator.Divide(divisionNumbers[0], divisionNumbers[1]));
+                        Console.WriteLine();
+                        break;
+
+                    case "5":
+                        return;
+
+
+                    default:
+                        Console.WriteLine("Unknown input");
+                        break;
+                }
+            }
+
+        }
+    }
+}
